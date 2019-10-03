de1c04e9b50c2eb0f7cc16b39d5f9c46c834d20a
EfrenAl <eal@tier.app>
2019-10-03 20:46:17 +0200

    DB instance

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-var middleware = require('./middleware')
+var bodyParser = require('body-parser');
+var _ = require('underscore')
+var db = require('./db.js')
+var middleware = require('./middleware');
 
 //We indicate the whole app to use middleware requireAuthentication
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
+app.use(bodyParser.json());
 
 
 app.get('/', function(req, res){
     res.send('Hello biatch');
 });
 
-app.get('/about', middleware.requireAuthentication, function(req, res){
-    res.send('About us');
+app.get('/user', middleware.requireAuthentication, function(req, res){
+    
 });
+
+app.post('/user', function(req, res){
+    var body = req.body;
+    db.user.create(body).then(function(user){
+        res.json(user.toJSON())
+    }, function(e){
+        res.status(400).json(e)
+    });
+
+
+})
+
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function(){
-    console.log('Express server started in port: ' + PORT)
-});
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log('Express server started in port: ' + PORT)
+    });
+})
+
+
+
